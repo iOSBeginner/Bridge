b369e89c2cd7f389f706da45f40928ad2e4324ab
Su, Jian-Hau <jenhausu@icloud.com>
2015-05-16 04:21:02 +0800

    init project

---
+++ Bridge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bridge
+//
+//  Created by 蘇健豪1 on 2015/5/16.
+//  Copyright (c) 2015年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Bridge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bridge
+//
+//  Created by 蘇健豪1 on 2015/5/16.
+//  Copyright (c) 2015年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
